1420ab6d50b4889a60150d5eb85c0625de99ee6c
mdubreuil <melanedhayden@gmail.com>
2017-11-26 21:32:19 +0100

    Adding Logs to see app lifecycle

--- app/src/main/java/com/example/epulapp/quizzandroid/QuizzActivity.java
+++ app/src/main/java/com/example/epulapp/quizzandroid/QuizzActivity.java
@@ -5,25 +5,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class QuizzActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("onCreateQuizz","Create quizz activity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizz);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }

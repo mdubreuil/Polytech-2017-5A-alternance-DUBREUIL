88e4bd01b1a1b4140268bb5a5157567eb137c985
mdubreuil <melanedhayden@gmail.com>
2017-11-26 21:21:24 +0100

    Adding logs to see app life cycle

--- app/src/main/java/com/example/epulapp/quizzandroid/MainQuizz.java
+++ app/src/main/java/com/example/epulapp/quizzandroid/MainQuizz.java
@@ -2,11 +2,13 @@ package com.example.epulapp.quizzandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainQuizz extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("onCreateMenu","Menu activity created");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_quizz);
     }
